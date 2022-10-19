9275c23723d96ae59388d1e4f2ac415722ffb764
Saurabh <Saurabhkr952@gmail.com>
2022-10-19 20:17:16 +0530

    script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,20 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
+def incrementVersion(){
+    echo 'Increment version'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+}
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
+    sh "docker build -t saurabhkr952/java-maven-app:$IMAGE_NAME ."
 } 
 
 def deployApp() {
     echo 'deploying the application...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push saurabhkr952/demo-app:$IMAGE_NAME"
+    }
 } 
 
 return this
